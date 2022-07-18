e3de76909ea989ad06931e1fc1df75cd00972868
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2022-07-18 20:05:52 +0530

    feat: openapi docs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import { Logger } from '@nestjs/common';
 
 if (process.env.IS_TS_NODE) {
@@ -7,11 +8,19 @@ if (process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-require('dotenv').config();
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Realworld')
+    .setDescription("The only ToDo demo you'll need to create")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
